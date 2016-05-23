01468cd8c01ae0319d39b418884c2dcfcba374dc
Kentaro Kawano <kawano.kentaro@synergy101.jp>
2016-05-23 12:56:31 +0900

    右下のキーはL2に変更

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  -   |           |  _   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | ~L1  |ZenHan|Alt   | LGui | Esc  |                                       |Enter |BkSpc |   [  |   ]  | ~L2  |
+ *   | ~L2  |ZenHan|Alt   | LGui | Esc  |                                       |Enter |BkSpc |   [  |   ]  | ~L2  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | Left |Right |       | Left | Right  |
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,    KC_E,   KC_R,   KC_T,   JKC_CIRC,
         KC_LCTL,        KC_A,         KC_S,    KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,    KC_C,   KC_V,   KC_B,   KC_MINS,
-        MO(SYMB),       KC_ZKHK,      KC_LALT, KC_LGUI,KC_ESC,
+        MO(MDIA),       KC_ZKHK,      KC_LALT, KC_LGUI,KC_ESC,
 
                                                KC_LEFT,                 KC_RGHT,
                                                                         KC_UP,
