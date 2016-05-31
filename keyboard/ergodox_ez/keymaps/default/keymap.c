03fb31cc3620dd9e06ebe6615899dc57e731aecd
Kentaro Kawano <kawano.kentaro@synergy101.jp>
2016-05-31 12:53:31 +0900

    イコールを設定

--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -33,6 +33,7 @@
 #define JKC_LPRN LSFT(KC_8)      // (
 #define JKC_RPRN LSFT(KC_9)      // )
 #define JKC_AMPR LSFT(KC_6)      // &
+#define JCK_EQL LSFT(KC_MINS)    // =
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -62,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,         KC_1,         KC_2,    KC_3,   KC_4,   KC_5,   KC_6,
         KC_TAB,         KC_Q,         KC_W,    KC_E,   KC_R,   KC_T,   JKC_CIRC,
-        CTL_T(KC_EQL),  KC_A,         KC_S,    KC_D,   KC_F,   KC_G,
+        CTL_T(JKC_EQL), KC_A,         KC_S,    KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,    KC_C,   KC_V,   KC_B,   KC_MINS,
         KC_LANG2,       KC_ZKHK,      KC_LALT, KC_LGUI,KC_ESC,
 
